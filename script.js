b35625935f706ac5a0974fc888b9d7629f4aaf67
Yen Tran <yen.tran@autodesk.com>
2016-11-03 08:35:35 -0700

    generateRandomColors() and randomColor()

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-var colors = [
-		'rgb(255, 0, 0)',
-		'rgb(255, 255, 0)',
-		'rgb(0, 255, 0)',
-		'rgb(0, 255, 255)',
-		'rgb(0, 0, 255)',
-		'rgb(255, 0, 255)'
-]
+var colors = generateRandomColors(6),
+		squares = document.querySelectorAll('.square'),
+		pickedColor = pickColor(),
+		colorDisplay = document.getElementById('colorDisplay'),
+		messageDisplay = document.querySelector('#message'),
+		h1 = document.querySelector('h1');
 
-var squares = document.querySelectorAll('.square'),
-		pickedColor = pickColor();
-		colorDisplay = document.getElementById('colorDisplay');
-		messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +17,11 @@ for (var i = 0; i < squares.length; i++) {
 		//grab color of clicked square
 		var clickedColor = this.style.background;
 		//compare color to picked color
+		console.log(clickedColor, pickedColor);
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = 'Correct!';
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 			this.style.background = '#232323';
 			messageDisplay.textContent = 'Try Again';
@@ -44,4 +40,26 @@ function changeColors(color){
 function pickColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = [];
+	//add num random colors to array
+	for(var i = 0; i < num; i++){
+		//get random color and push into arr
+		arr.push(randomColor())
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor(){
+	//pick a 'red' from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a 'green' from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a 'blue' from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
