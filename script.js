1cb2a9c706b2081404ad15fd8eea0a51ed4f80d0
Yen Tran <yen.tran@autodesk.com>
2016-11-03 09:37:47 -0700

    adds easy and hard mode

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-var colors = generateRandomColors(6),
+var numSquares = 6,
+		colors = generateRandomColors(numSquares),
 		squares = document.querySelectorAll('.square'),
 		pickedColor = pickColor(),
 		colorDisplay = document.getElementById('colorDisplay'),
 		messageDisplay = document.querySelector('#message'),
 		h1 = document.querySelector('h1'),
-		resetButton = document.querySelector('#reset');
+		resetButton = document.querySelector('#reset'),
+		easyBtn = document.querySelector('#easyBtn'),
+		hardBtn = document.querySelector('#hardBtn');
+
+
+easyBtn.addEventListener('click', function(){
+	easyBtn.classList.add('selected');
+	hardBtn.classList.remove('selected');
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		if(colors[i]) {
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = 'none';
+		}
+	}
+});
+
+hardBtn.addEventListener('click', function(){
+	hardBtn.classList.add('selected');
+	easyBtn.classList.remove('selected');
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+			squares[i].style.background = colors[i];
+			squares[i].style.display = 'block';
+	}
+});
 
 resetButton.addEventListener('click', function(){
 	//generate all new color
-	colors = generateRandomColors(6)
+	colors = generateRandomColors(numSquares);
 	//pick new random color from array
 	pickedColor = pickColor();
 	//change colorDisplay to match picked color
